a2cc1ac730b618b5bda25c58fc3491f1c5c54cd9
Hank Stoever <hstove@gmail.com>
2012-12-12 13:42:26 -0800

    added tests

--- objective-dcTests/objective_dcTests.m
+++ objective-dcTests/objective_dcTests.m
@@ -7,6 +7,9 @@
 //
 
 #import "objective_dcTests.h"
+#import "DCClient.h"
+#import "DCUser.h"
+#import "DCURLParser.h"
 
 @implementation objective_dcTests
 
@@ -24,9 +27,50 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testInitSharedClient
 {
-    STFail(@"Unit tests are not implemented yet in objective-dcTests");
+    DCClient *dailycred = [DCClient initWithClientId:@"client_id" andClientSecret:@"client_secret" withRedirectUri:@"myapp://url"];
+    STAssertEquals(@"client_id", dailycred.clientId, @"client_id should be set");
+    STAssertEquals(@"client_secret", dailycred.clientSecret,@"client_secret should be set");
+    STAssertEquals(@"myapp://url", dailycred.redirectUri,@"redirect_uri should be set");
+    //make sure the shared client got set correctly too
+    dailycred = [DCClient sharedClient];
+    STAssertEquals(@"client_id", dailycred.clientId, @"client_id should be set");
+    STAssertEquals(@"client_secret", dailycred.clientSecret,@"client_secret should be set");
+    STAssertEquals(@"myapp://url", dailycred.redirectUri,@"redirect_uri should be set");
+}
+
+//To keep track of current user, DCClient serializes the user into NSUserDefaults
+//This requires custom encoding, so lets test that all of the properties get passed along correctly
+-(void)testUserPersistance{
+    DCUser *user = [[DCUser alloc] init];
+    user.email = @"test@2.com";
+    user.display = @"testuser";
+    user.picture = @"https://www.dailycred.com/woah";
+    user.uuid = @"dc-id";
+    NSDictionary *dummyDictionary = [[NSDictionary alloc] initWithObjectsAndKeys:@"attribute",@"value", nil];
+    user.identities = dummyDictionary;
+    user.json = dummyDictionary;
+    user.accessTokens = dummyDictionary;
+    [DCClient setCurrentUser:user];
+    user = [DCClient getCurrentUser];
+    STAssertEquals(@"test@2.com", user.email,@"email should be persisted");
+    STAssertEquals(@"testuser", user.display,@"display should be persisted");
+    STAssertEquals(@"https://www.dailycred.com/woah", user.picture,@"picture should be persisted");
+    STAssertEquals(@"dc-id", user.uuid,@"uuid should be persisted");
+    STAssertEquals(dummyDictionary, user.accessTokens,@"access tokens should be persisted");
+    STAssertEquals(dummyDictionary, user.json,@"json should be persisted");
+    STAssertEquals(dummyDictionary, user.identities,@"identities should be persisted");
+}
+
+-(void)testURLUtils{
+    DCClient *dailycred = [DCClient initWithClientId:@"client_id" andClientSecret:@"client_secret" withRedirectUri:@"myapp://url"];
+    DCURL *url = [dailycred getURLFromEndpoint:kMeJsonEndpoint];
+    STAssertEqualObjects(@"https://www.dailycred.com/graph/me.json",[url absoluteString],@"url should construct correctly");
+    url = [dailycred getAuthURLFromEndpoint:kConnectEndpoint];
+    DCURLParser *parser = [[DCURLParser alloc] initWithURLString:[url absoluteString]];
+    STAssertEqualObjects([parser valueForVariable:@"client_id"], @"client_id",@"client_id should be set");
+    STAssertEqualObjects([parser valueForVariable:@"redirect_uri"], @"myapp://url",@"redirect_uri should be set");
 }
 
 @end
